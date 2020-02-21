a249e6868964924a9b85b13633f220fc4442c9ef
hima govind <himabindu.g23@gmail.com>
2020-02-21 07:00:02 +0000

    add cloudformation templates for emo

--- ec2Create_first.tf
+++ ec2Create_first.tf
@@ -3,7 +3,14 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+# SSH key pair for accessing ec2 machine
+/* resource "aws_key_pair" "sshKeyPair" {
+  key_name   = "WindowsAccessKeys"
+  public_key = "${var.public_ssh_key}"
+} */
+
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  count = 1;  
+  ami  = "ami-2757f631"
   instance_type = "t2.micro"
 }
